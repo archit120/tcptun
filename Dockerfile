da09ac938e4e0ea2bdab251fc6715d2996f655aa
Archit Rungta <architrungta120@gmail.com>
2022-02-14 16:14:04 +0530

    Initial experimentation with TUN

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM golang:1.17-bullseye
+
+RUN apt update && apt install iproute2
 WORKDIR /code
 
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/songgao/water"
+)
 
 func main() {
-	fmt.Println("hello world")
+	config := water.Config{
+		DeviceType: water.TUN,
+	}
+	// config.Name = "O_O"
+
+	ifce, err := water.New(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	packet := make([]byte, 2000)
+
 	for {
+		n, err := ifce.Read(packet)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Dst: %x\n", packet[:n])
 	}
 }
