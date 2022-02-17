0fd4f897d7abc6a2fee955df19851b06aaf02e4d
Archit Rungta <architrungta120@gmail.com>
2022-02-17 22:26:18 +0530

    more script cahnges

--- scripts/client_p1.sh
+++ scripts/client_p1.sh
@@ -2,6 +2,6 @@
 
 # ${client IP} ${device name} ${server ip} ${server gateway}
 
-ip link set dev $1 mtu 1400
-ip link set dev $1 up
-ip r
+# ip link set dev $1 mtu 1400
+# ip link set dev $1 up
+# ip r
